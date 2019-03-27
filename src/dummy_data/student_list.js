ca150a8c384ae2b821c2fb583178d8ee75fd69a3
Scott Bowler <scott.bowler@learningfuze.com>
2019-03-27 12:54:32 -0700

    Completed displaying dummy student data

---
+++ src/dummy_data/student_list.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        name: 'Clark Kent',
+        course: 'Flying 101',
+        grade: '98',
+        id: 1
+    },
+    {
+        name: 'Kelly Smith',
+        course: 'English 301',
+        grade: '92',
+        id: 2
+    },
+    {
+        name: 'Bruce Wayne',
+        course: 'Criminal Justice',
+        grade: '100',
+        id: 3
+    },
+    {
+        name: 'Peter Parker',
+        course: 'Photography 98',
+        grade: '84',
+        id: 4
+    },
+    {
+        name: 'Amanda Jones',
+        course: 'Math Things 402',
+        grade: '97',
+        id: 5
+    }
+];
